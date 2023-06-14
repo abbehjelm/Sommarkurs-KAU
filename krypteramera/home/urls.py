8825bd6e229061ebdb2a3f5c505465aaecc7557b
abbehjelm <abbehjelm@gmail.com>
2023-06-14 11:31:06 +0200

    init commit

---
+++ krypteramera/home/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+from django.urls import path, re_path
+from home import views
+
+
+urlpatterns = [
+    re_path(r'^$', views.home, name='home'),
+    re_path(r'^home$', views.home, name='home'),
+]
