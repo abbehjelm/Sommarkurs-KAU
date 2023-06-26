7146288231222bec25b96fdb90d7992857ff017a
abbehjelm <abbehjelm@gmail.com>
2023-06-26 15:16:49 +0200

    worked on AES CBC mode

--- krypteramera/home/forms.py
+++ krypteramera/home/forms.py
@@ -1,17 +1,14 @@
 from django import forms
 
 
+
+
 class EncryptionForm(forms.Form):
-    key = forms.CharField(max_length=44, min_length=3, widget=forms.PasswordInput(attrs={
+    key = forms.CharField(max_length=32, min_length=3, widget=forms.PasswordInput(attrs={
         'class': 'form-control',
         'id': 'encryptionkey',
         'placeholder': 'Ange din önskade krypteringsnyckel...'
-    }))
-    iv = forms.CharField(max_length=16, min_length=3, required=False, widget=forms.TextInput(attrs={
-        'class': 'form-control',
-        'id': 'initVector',
-        'placeholder': 'Ange en startvektor (valfritt men rekommenderas) ...'
-    }))
+    }))   
     text = forms.CharField(max_length=1000, min_length=3, widget=forms.Textarea(attrs={
         'class': 'form-control',
         'placeholder': 'Vad vill du kryptera idag...',
@@ -20,16 +17,16 @@ class EncryptionForm(forms.Form):
 
 
 class DecryptionForm(forms.Form):
-    key = forms.CharField(max_length=44, min_length=3, widget=forms.PasswordInput(attrs={
+    key = forms.CharField(max_length=32, min_length=3, widget=forms.PasswordInput(attrs={
         'class': 'form-control',
-        'id': 'encryptionkey',
+        'id': 'encryptionkeyDecrypt',
         'placeholder': 'Ange krypteringsnyckeln...'
 
     }))
-    iv = forms.CharField(max_length=16, min_length=3, required=False,widget=forms.TextInput(attrs={
+    iv = forms.CharField(max_length=32, min_length=3, required=False,widget=forms.TextInput(attrs={
         'class': 'form-control',
         'id': 'initVector',
-        'placeholder': 'Ango startvektor (om användes vid kryptering)...'
+        'placeholder': 'Ange startvektorn som genererades vid krypteringen...'
     }))
     cipher = forms.CharField(max_length=1000, min_length=3, widget=forms.Textarea(
         attrs={
