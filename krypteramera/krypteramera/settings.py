eeb986c5a23010b86c742d13e10ec70a5c1e9c5a
abbehjelm <abbehjelm@gmail.com>
2023-06-26 07:42:39 +0200

    added static file settings for production

--- krypteramera/krypteramera/settings.py
+++ krypteramera/krypteramera/settings.py
@@ -122,4 +122,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
+# Development settings
+# STATIC_URL = '/static/'
+
+# Live setiings
+STATIC_URL = '/home/abbehjelm/Sommarkurs-KAU/static'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
